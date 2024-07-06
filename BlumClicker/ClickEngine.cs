d3ba83c2c0c880e40e396c537eda51f93f548f9c
Sk0rd1 <tyschenko.mykhailo@gmail.com>
2024-07-06 10:38:10 +0300

    Initialization

---
+++ BlumClicker/ClickEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using MessageBox = System.Windows.MessageBox;
+using System.Windows;
+using System.Diagnostics;
+using System.Threading;
+
+namespace BlumClicker
+{
+    internal class ClickEngine
+    {
+        private const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwflags, uint dx, uint dy, uint dwData, uint ExtraInf);
+
+        [DllImport("user32.dll")]
+        private static extern void SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        int[] colorMin = { 40, 200, -1 };
+        int[] colorMax = { 120, 256, 80 };
+
+        bool isSearch = true;
+        Thread searchThread;
+
+        public void Start()
+        {
+            isSearch = true;
+
+            searchThread = new Thread(SearchPoints);
+            searchThread.Start();
+        }
+
+        private void SearchPoints()
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            while (isSearch)
+            {
+                System.Threading.Thread.Sleep(1);
+
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int x = 0; x < 470; x++)
+                    {
+                        int yValue = 370 + y * 100;
+                        Color currentPixelColor = bitmap.GetPixel(x, yValue);
+
+                        if (!(currentPixelColor.R < 40 && currentPixelColor.G < 40 && currentPixelColor.B < 40))
+                        {
+                            Click(x, yValue);
+                            x += 20;
+                        }
+                    }
+                }
+
+                Color p1 = bitmap.GetPixel(80, 750);
+                Color p2 = bitmap.GetPixel(380, 750);
+
+                if (p1.R > 200 && p1.G > 200 && p1.B > 200 && p2.R > 200 && p2.G > 200 && p2.B > 200)
+                    Click(80, 750);
+
+            }
+                bitmap.Dispose();
+        }
+        private void Click(int x, int y)
+        {
+            POINT currentPosition;
+            GetCursorPos(out currentPosition);
+
+            SetCursorPos(x, y);
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            System.Threading.Thread.Sleep(1);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+            //SetCursorPos(currentPosition.X, currentPosition.Y);
+
+            System.Threading.Thread.Sleep(1);
+        }
+
+        public void Stop() 
+        {
+            isSearch = false;
+            if (searchThread != null && searchThread.IsAlive)
+            {
+                searchThread.Join();
+            }
+        }
+    }
+}
---
+++ BlumClicker/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlumClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClickEngine clickEngine;
+        bool isActive = false;
+        CancellationTokenSource cancellationTokenSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            clickEngine = new ClickEngine();
+
+            HotkeysManager.SetupSystemHook();
+
+            GlobalHotkey firstHotkey = new GlobalHotkey(ModifierKeys.Control, Key.F2, StartHotkey);
+            GlobalHotkey secondHotkey = new GlobalHotkey(ModifierKeys.Control, Key.F4, StopHotkey);
+            HotkeysManager.AddHotkey(firstHotkey);
+            HotkeysManager.AddHotkey(secondHotkey);
+
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            HotkeysManager.ShutdownSystemHook();
+        }
+
+        private void OnbClick(object sender, RoutedEventArgs e)
+        {
+            if(isActive)
+            {
+                bClick.Content = "Start(Ctrl+F2)";
+                clickEngine.Stop();
+                //cancellationTokenSource.Cancel();
+            }
+            else
+            {
+                bClick.Content = "Stop(Ctrl+F4)";
+                clickEngine.Start();
+                cancellationTokenSource = new CancellationTokenSource();
+                //StartClickingAsync(cancellationTokenSource.Token);
+            }
+            isActive = !isActive;
+        }
+
+        public void StartHotkey()
+        {
+            if(!isActive)
+            {
+                bClick.Content = "Stop(Ctrl+F4)";
+                clickEngine.Start();
+                isActive = !isActive;
+            }
+        }
+
+        public void StopHotkey()
+        {
+            if (isActive)
+            {
+                bClick.Content = "Start(Ctrl+F2)";
+                clickEngine.Stop();
+                isActive = !isActive;
+            }
+        }
+
+        private async Task StartClickingAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                clickEngine.Start();
+                await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+                clickEngine.Stop();
+
+                Dispatcher.Invoke(() =>
+                {
+                    bClick.Content = "Start(Ctrl+F2)";
+                    isActive = !isActive;
+                });
+            }
+            catch (TaskCanceledException) 
+            {
+
+            }
+        }
+    }
+}
